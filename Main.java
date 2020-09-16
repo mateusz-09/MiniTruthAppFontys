9de34fa647d36457e7f316898963f032dd18e6d1
mateusz-09 <71397486+mateusz-09@users.noreply.github.com>
2020-09-16 19:50:15 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,12 +1,13 @@
+package com.company;
+
+import java.util.Scanner;
+
 public class Main {
-
+//  Word Counter
     public static int countWords(String s){
-
         int wordCount = 0;
-
         boolean word = false;
         int endOfLine = s.length() - 1;
-
         for (int i = 0; i < s.length(); i++) {
             // if the char is a letter, word = true.
             if (Character.isLetter(s.charAt(i)) && i != endOfLine) {
@@ -24,15 +25,14 @@ public class Main {
         }
         return wordCount;
     }
-
+//  Text Reducer
     public static String reducedText(String s, int maxWords){
         int spaceCount = 0;
         boolean word = false;
         int endOfLine = s.length() - 1;
-        String[] stringSplitted = new String[100];//assuming the sentence has 100 words or less, you can change the value to Integer.MAX_VALUE instead of 10
-        int stringLength=0;//this will give the character count in the string to be split
+        String[] stringSplitted = new String[100];  //assuming the sentence has 100 words or less, you can change the value to Integer.MAX_VALUE instead of 10
+        int stringLength=0;    //this will give the character count in the string to be split
         int lastIndex=0;
-
         // String is limited to maxWords
         for(int i=0;i<s.length();i++){
             if(s.charAt(i)==' '){   //check whether the character is a space, if yes then count the words
@@ -40,25 +40,53 @@ public class Main {
             }
             if(spaceCount==100){     //after encountering 10 words split the sentence from lastIndex to the 10th word. For the first time lastIndex would be zero that is starting position of the string
                 stringSplitted[stringLength++] = s.substring(lastIndex, i);
-                lastIndex=i;// to get the next part of the sentence, set the last index to 10th word
-                spaceCount=0;//set the number of spaces to zero to starting counting the next 10 words
+                lastIndex=i;    // to get the next part of the sentence, set the last index to 10th word
+                spaceCount=0;   // set the number of spaces to zero to starting counting the next 10 words
                 System.out.println(stringSplitted[0]);
             }
         }
-        int dotss = stringSplitted[0].indexOf(".");
-        int dots = stringSplitted[0].lastIndexOf(".");
         return stringSplitted[0];
-
-     }
-
+    }
     public static void main(String[] args) {
+//      User input
+        Scanner textReader = new Scanner(System.in);
+        System.out.println("Voer uw tekst in");
+        String textInput = textReader.nextLine();
 
-        String story = "Nunc nulla, pulvinar donec at fringilla amet. Non a ridiculus duis aenean sociis dictum penatibus torquent natoque sit duis imperdiet elit eget lobortis etiam natoque congue facilisi et placerat iaculis maecenas ridiculus malesuada taciti velit vehicula vehicula sociis nisi est duis arcu habitant tincidunt feugiat, maecenas habitant nonummy pede pulvinar parturient etiam taciti integer massa venenatis sem tincidunt. Eget. Pretium bibendum urna euismod malesuada sem facilisi. Sodales cum nisi taciti id tortor cras nisi congue ornare massa purus cubilia dignissim cum, dui. Laoreet blandit aenean porttitor. Est dictum vehicula Nam cum blandit scelerisque vulputate netus amet urna viverra etiam proin Scelerisque fusce iaculis. Pretium pretium natoque lacus vulputate vitae libero velit suspendisse nisi id eu. Ornare mattis risus scelerisque, blandit Habitasse. Cubilia velit sapien viverra mauris hendrerit. Urna egestas suscipit condimentum, nascetur phasellus a platea mollis eu dui condimentum in est Faucibus a. Maecenas integer imperdiet erat cras. Sollicitudin ad congue fringilla malesuada conubia faucibus facilisis id sed egestas. Porttitor torquent accumsan. Eget eget. Pharetra placerat sapien potenti montes morbi magna curabitur iaculis tempus class. Nulla. Sodales ac. Faucibus nunc. Interdum venenatis pede molestie volutpat. Nascetur senectus etiam fermentum feugiat tincidunt ornare. Suspendisse ad consectetuer, laoreet vel metus facilisis malesuada.";
-        int numberOfWords = countWords(story);
-        System.out.println("String contains: " + numberOfWords + " words");
-        if(numberOfWords > 10){
-            reducedText(story,100);
+//      Testinput: "Nunc nulla, pulvinar donec at fringilla amet. Non a ridiculus duis aenean sociis dictum penatibus torquent natoque sit duis imperdiet elit eget lobortis etiam natoque congue facilisi et placerat iaculis maecenas ridiculus malesuada taciti velit vehicula vehicula sociis nisi est duis arcu habitant tincidunt feugiat, maecenas habitant nonummy pede pulvinar parturient etiam taciti integer massa venenatis sem tincidunt. Eget. Pretium bibendum urna euismod malesuada sem facilisi. Sodales cum nisi taciti id tortor cras nisi congue ornare massa purus cubilia dignissim cum, dui. Laoreet blandit aenean porttitor. Est dictum vehicula Nam cum blandit scelerisque vulputate netus amet urna viverra etiam proin Scelerisque fusce iaculis. Pretium pretium natoque lacus vulputate vitae libero velit suspendisse nisi id eu. Ornare mattis risus scelerisque, blandit Habitasse. Cubilia velit sapien viverra mauris hendrerit. Urna egestas suscipit condimentum, nascetur phasellus a platea mollis eu dui condimentum in est Faucibus a. Maecenas integer imperdiet erat cras. Sollicitudin ad congue fringilla malesuada conubia faucibus facilisis id sed egestas. Porttitor torquent accumsan. Eget eget. Pharetra placerat sapien potenti montes morbi magna curabitur iaculis tempus class. Nulla. Sodales ac. Faucibus nunc. Interdum venenatis pede molestie volutpat. Nascetur senectus etiam fermentum feugiat tincidunt ornare. Suspendisse ad consectetuer, laoreet vel metus facilisis malesuada."
+
+//      Big Brother check
+//        for loop
+        String textInputControl = textInput;
+        if (textInputControl.contains("Big") && textInputControl.contains("Brother")){
+            if (textInputControl.indexOf("Big") < textInputControl.indexOf("Brother")){
+                String textInputControlCheck = textInputControl.substring(textInputControl.indexOf("Big")); // Remove everything before Big
+                textInputControlCheck = textInputControlCheck.substring(0, (textInputControlCheck.lastIndexOf("Brother")+7)); // Remove everything after Brother
+                if (countWords(textInputControlCheck) <= 5) { // Less or 3 words between triggers ALARM
+                    System.out.println("ALARM, STOP THE PROGRAM");
+                }
+                else { // Check failed, update textInputControl
+                    
+                }
+            }
         }
+
+//      Count words
+        int numberOfWords = countWords(textInput);
+        System.out.println("String contains: " + numberOfWords + " words");
+
+        if(numberOfWords > 100){
+            System.out.println("String contains too many words, it must be reduced");
+            textInput = reducedText(textInput,100);
+            textInput = textInput.substring(0, (textInput.lastIndexOf(".") + 1)); // Remove everything after the last dot.
+            System.out.println("Reduced string: " + textInput);
+            numberOfWords = countWords(textInput);
+            System.out.println("This new string contains: " + numberOfWords + " words");
+        }
+
+
+
+
 
 //        // write your code here
 //        String sentence = "Check this answer and you can find the keyword with this code Big Brother";
@@ -75,8 +103,11 @@ public class Main {
 //            System.out.println("not found");
 //
 //        }
-
-
-
     }
 }
+
+
+
+
+
+
